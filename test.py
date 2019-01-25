cc12b7d7f1802e3d4938e63a8601e228dab6e5d9
dengzeshuai <1321730442@qq.com>
2019-01-25 11:04:57 +0800

    change input and save path

--- test.py
+++ test.py
@@ -1,23 +1,24 @@
 import sys
-import os.path
+import os
 import glob
 import cv2
 import numpy as np
 import torch
 import architecture as arch
-
 model_path = sys.argv[1]  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
+testset = sys.argv[2] # Set5, Set14, B100, Urban100, Manga109
 device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
 
-test_img_folder = 'LR/*'
+test_img_folder = '/home/datasets/sr/benchmark/{}/LR_bicubic/X4/*'.format(testset)
+save_path = 'results/{}/'.format(testset)
 
 model = arch.RRDB_Net(3, 3, 64, 23, gc=32, upscale=4, norm_type=None, act_type='leakyrelu', \
                         mode='CNA', res_scale=1, upsample_mode='upconv')
 model.load_state_dict(torch.load(model_path), strict=True)
 model.eval()
-for k, v in model.named_parameters():
-    v.requires_grad = False
+# for k, v in model.named_parameters():
+#     v.requires_grad = False
 model = model.to(device)
 
 print('Model path {:s}. \nTesting...'.format(model_path))
@@ -33,8 +34,10 @@ for path in glob.glob(test_img_folder):
     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
     img_LR = img.unsqueeze(0)
     img_LR = img_LR.to(device)
-
-    output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+    with torch.no_grad():
+        output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
-    cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    cv2.imwrite(os.path.join(save_path, '{:s}.png'.format(base)), output)
